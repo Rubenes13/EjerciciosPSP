d3b71d4ea6eea50bfaae18b99acf1ed5fa4612d8
Rubenes13 <rubensanchez3042@gmail.com>
2025-10-30 10:14:56 +0100

    Add pipe communication between parent and child processascas

--- EjercicioPipe.c
+++ EjercicioPipe.c
@@ -6,6 +6,7 @@ int main() {
     int fd[2];
     char buffer[30];
     pid_t pid;
+    int salida;
 
     pipe(fd);
     pid=fork();
@@ -14,17 +15,21 @@ int main() {
     {
         case -1 : //Error
             printf("Error al crear el proceso hijo\n");
+            salida = -1;
             break;
         case 0 : //Proceso hijo
             printf("El hijo escribe en la pipe\n");
             write(fd[1], "Hola papa", 10);
             close(fd[0]); //Cerramos el extremo de lectura
+            salida=1;
             break;
         default:
             wait(NULL); //Esperamos a que termine el hijo
             printf("El padre lee de la pipe\n");
             read(fd[0], buffer, sizeof(buffer));
             printf("Mensaje leido: %s\n", buffer);
+            salida=1;
             break;
     }
+    return salida;
 }
