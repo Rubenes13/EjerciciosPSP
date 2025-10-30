8986e1575f6e4b70bf58f87c3894f51dda83ca02
Rubenes13 <rubensanchez3042@gmail.com>
2025-10-30 10:13:59 +0100

    ascsa

--- EjercicioPipe.c
+++ EjercicioPipe.c
@@ -12,19 +12,19 @@ int main() {
 
     switch (pid)
     {
-    case -1 : //Error
-        printf("Error al crear el proceso hijo\n");
-        break;
-    case 0 : //Proceso hijo
-        printf("El hijo escribe en la pipe\n");
-        write(fd[1], "Hola papa", 10);
-        close(fd[0]); //Cerramos el extremo de lectura
-        break;
-    default:
-        wait(NULL); //Esperamos a que termine el hijo
-        printf("El padre lee de la pipe\n");
-        read(fd[0], buffer, sizeof(buffer));
-        printf("Mensaje leido: %s\n", buffer);
-        break;
+        case -1 : //Error
+            printf("Error al crear el proceso hijo\n");
+            break;
+        case 0 : //Proceso hijo
+            printf("El hijo escribe en la pipe\n");
+            write(fd[1], "Hola papa", 10);
+            close(fd[0]); //Cerramos el extremo de lectura
+            break;
+        default:
+            wait(NULL); //Esperamos a que termine el hijo
+            printf("El padre lee de la pipe\n");
+            read(fd[0], buffer, sizeof(buffer));
+            printf("Mensaje leido: %s\n", buffer);
+            break;
     }
 }
