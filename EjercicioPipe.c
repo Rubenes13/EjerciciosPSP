2e9287c2d34de95f98648c53f5b2fd4b4069fa6a
Ruben <rubensanchez3042@gmail.com>
2025-10-30 09:49:23 +0100

    Ejercicio de pipes en C

---
+++ EjercicioPipe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int fd[2];
+    char buffer[30];
+    pid_t pid;
+
+    pipe(fd);
+    pid=fork();
+
+    switch (pid)
+    {
+    case -1 : //Error
+        printf("Error al crear el proceso hijo\n");
+        exit(-1);
+        break;
+    case 0 : //Proceso hijo
+        printf("El hijo escribe en la pipe\n");
+        write(fd[1], "Hola papa", 10);
+        close(fd[0]); //Cerramos el extremo de lectura
+        break;
+    default:
+        wait(NULL); //Esperamos a que termine el hijo
+        printf("El padre lee de la pipe\n");
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Mensaje leido: %s\n", buffer);
+        break;
+    }
+}
