cd746dbc71f0148b21dc9dcc0c0364118b527402
Ruben <rubensanchez3042@gmail.com>
2025-11-06 09:18:54 +0100

    Ejer2

--- Ejercicio1-2.c
+++ Ejercicio1-2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
     int fd[2];
     char buffer[30];
     pid_t pid;
     int salida;
+    char quienSoy[15];
 
     pipe(fd);
     pid=fork();
@@ -19,8 +21,10 @@ int main() {
             break;
         case 0 : //Proceso hijo
             close(fd[0]); //Cerramos el extremo de lectura
+            strcpy(quienSoy, "Proceso Hijo");
+            char mensaje[] = "Buenos dias padre";
             printf("El hijo escribe algo en la pipe\n");
-            write(fd[1], "Buenos dias padre", 18);
+            write(fd[1], mensaje, sizeof(mensaje));
             close(fd[1]); //Cerramos el extremo de escritura
             salida=1;
             printf("El hijo ha terminado de escribir\n");
@@ -28,6 +32,7 @@ int main() {
         default:
             wait(NULL); //Esperamos a que termine el hijo
             close(fd[1]); //Cerramos el extremo de escritura
+            strcpy(quienSoy, "Proceso Padre");
             printf("El padre lee de la pipe: ");
             read(fd[0], buffer, sizeof(buffer));
             printf("Mensaje leido: %s\n", buffer);
@@ -35,6 +40,6 @@ int main() {
             printf("El padre ha terminado de leer\n");
             break;
     }
-    printf("Fin del programa\n");
+    printf("Fin del %s\n", quienSoy);
     return salida;
 }
